11aaf09776a763c681f4ba92c7a4f420dbb44b20
Ilya Plyusnin <ilyaplyusnin57@gmail.com>
2023-08-11 07:29:43 -0400

    Refactor contact-me into its own component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from "./components/navbar/Navbar";
 import Introduction from "./components/introduction/Introduction";
 import AboutMe from "./components/about_me/AboutMe";
 import Projects from "./components/projects/Projects";
+import ContactMe from "./components/contact_me/ContactMe";
 
 function App() {
   return (
@@ -12,18 +13,7 @@ function App() {
       <Introduction />
       <AboutMe />
       <Projects />
-      <section id="contact-me" className="container">
-        <div>
-          <h1>Contact Me</h1>
-          <section>
-            <p>You can email me at ilyaplyusnin57@gmail.com</p>
-            <p>
-              Just type in your email and message below and it will be sent to
-              my email
-            </p>
-          </section>
-        </div>
-      </section>
+      <ContactMe />
     </>
   );
 }
