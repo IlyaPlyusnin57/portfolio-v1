66c5d22547927ed47fe0a4837b2342d6d2eb2f08
Ilya Plyusnin <ilyaplyusnin57@gmail.com>
2023-08-22 02:05:00 -0400

    Update import path

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Navbar from "./components/navbar/Navbar";
 import Introduction from "./components/introduction/Introduction";
 import AboutMe from "./components/about_me/AboutMe";
 import Projects from "./components/projects/Projects";
-import Footer from "./components/footer/footer";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
