c22a3d97ad0247d31aaca6cdbbee6de31370706a
Ilya Plyusnin <ilyaplyusnin57@gmail.com>
2023-08-21 23:32:22 -0400

    Create a Footer component

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import "./Footer.scss";
+
+function Footer() {
+  return (
+    <section className="footer">
+      Coded in VS Code by yours truly. Built with React JS and Sass, deployed
+      with Vercel
+    </section>
+  );
+}
+
+export default Footer;
